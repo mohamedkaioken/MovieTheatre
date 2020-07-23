d19213697ce5f7c1f4277e3fa2e812846e23b88a
Mohamed <mohamed.m201139@gmail.com>
2020-07-23 17:58:11 +0200

    Editing Grid System

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer
-    v-if="loggedIn"
+      v-if="loggedIn"
       v-model="drawer"
       app
       clipped
@@ -24,86 +24,71 @@
               <v-icon>{{ item.icon }}</v-icon>
             </v-list-item-action>
             <v-list-item-content>
-              <v-list-item-title >{{
-                item.text
-              }}</v-list-item-title>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </template>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      v-if="loggedIn"
-      clipped-left
-      color="black"
-    >
+    <v-app-bar app v-if="loggedIn" clipped-left color="black">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title id="ff">Movie Theater</v-toolbar-title>
-       <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
       <v-btn id="ff" v-if="loggedIn" text @click="TryLogout">
         Log Out
       </v-btn>
     </v-app-bar>
 
     <v-content id="bg">
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-fade-transition mode="out-in" hide-on-leave>
-              <router-view >
-            </router-view>
-            </v-fade-transition>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-fade-transition mode="out-in" hide-on-leave>
+            <router-view> </router-view>
+          </v-fade-transition>
+        </v-row>
       </v-container>
     </v-content>
-
-   
   </v-app>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
 export default {
-  name: 'App',
+  name: "App",
 
-  mounted(){
-  },
+  mounted() {},
 
-   data: () => ({
-      drawer: null,
-      user:{},
-      items: [
+  data: () => ({
+    drawer: null,
+    user: {},
+    items: [
       {
         icon: "mdi-order-bool-descending",
         text: "Home",
-        route: "/"
+        route: "/",
       },
-      
     ],
-    }),
-    methods:{
-      ...mapActions('auth', ['logout']),
-      TryLogout(){
-        this.logout()
-      }
+  }),
+  methods: {
+    ...mapActions("auth", ["logout"]),
+    TryLogout() {
+      this.logout();
     },
-    computed:{
-      ...mapGetters('auth', [
-            'loggedIn'
-        ])
-    },
-    created () {
-      this.$vuetify.theme.dark = true
-    },
+  },
+  computed: {
+    ...mapGetters("auth", ["loggedIn"]),
+  },
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
 };
 </script>
 <style scoped>
-#bg{
-background-image: url('././assets/BG.png');
+#bg {
+  background-image: url("././assets/BG.png");
 }
-#ff{
-  font-family: 'Courier New', Courier, monospace;
+#ff {
+  font-family: "Courier New", Courier, monospace;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row class="mb-4" align-content="center" justify="center">
-      <v-col cols="12" xl="12" lg="6" md="12" sm="12" xs="12">
+      <v-col cols="6">
         <v-text-field
           label="Search"
           v-model="movie"
@@ -11,63 +11,54 @@
           hide-details
           @keyup="GetMovies"
         ></v-text-field>
-        <v-col>
-          <v-card
-            color="purple"
-            dark
-            style="max-width: 400px;"
-            v-for="result in results"
-            :key="result.title"
-            v-show="ShowForm"
-          >
-            <v-row>
-              <v-col cols="8">
-                <v-card-title>
-                  <div>
-                    <div class="headline">{{ result.title }}</div>
-                    <div>
-                      <v-expansion-panels>
-                        <v-expansion-panel class="transparent">
-                          <v-expansion-panel-header
-                            >Over View</v-expansion-panel-header
-                          >
-                          <v-expansion-panel-content>
-                            {{ result.overview }}
-                          </v-expansion-panel-content>
-                        </v-expansion-panel>
-                      </v-expansion-panels>
-                    </div>
-                    <div>{{ result.release_date }}</div>
-                  </div>
-                </v-card-title>
-              </v-col>
-              <v-img
-                class="shrink ma-2"
-                contain
-                height="125px"
-                :src="src + result.backdrop_path"
-                style="flex-basis: 125px"
-              ></v-img>
-            </v-row>
-            <v-divider dark></v-divider>
-            <v-card-actions class="pa-4">
-              Rate this album
-              <v-spacer></v-spacer>
-              <span class="grey--text text--lighten-2 caption mr-2">
-                ({{ rating }})
-              </span>
-              <v-rating
-                v-model="rating"
-                background-color="white"
-                color="yellow accent-4"
-                dense
-                half-increments
-                hover
-                size="18"
-              ></v-rating>
-            </v-card-actions>
-          </v-card>
-        </v-col>
+      </v-col>
+    </v-row>
+    <v-row justify="center text-center mt-4" v-show="ShowForm">
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+        v-for="result in results"
+        :key="result.title"
+      >
+        <v-card class="mx-auto" max-width="344">
+          <v-img :src="src + result.backdrop_path" height="200px"></v-img>
+
+          <v-card-title>
+            {{ result.title }}
+          </v-card-title>
+
+          <v-card-subtitle>
+            {{ new Date(result.release_date).getFullYear() }}
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn text>Share</v-btn>
+
+            <v-btn color="purple" text>
+              Explore
+            </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon @click="show = !show">
+              <v-icon>{{
+                show ? "mdi-chevron-up" : "mdi-chevron-down"
+              }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                {{ result.overview }}
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -79,7 +70,6 @@ export default {
   name: "Home",
 
   mounted() {
-    
     //   ApiService.get(
     //   `https://api.themoviedb.org/3/search/movie?api_key=18ba471261d253fb5c574c6f1de06e76&query="+${this.movie}"`
     // ).then((r) => {
@@ -95,41 +85,36 @@ export default {
     //   }
     //   console.log(this.results);
     // });
-
-    
   },
   data: () => ({
+    show: false,
     movies: [],
     results: [],
     descriptions: [],
-    movie: '',
-    ShowForm:false,
+    movie: "",
+    ShowForm: false,
     src: "https://image.tmdb.org/t/p/original",
     rating: 0.0,
   }),
   methods: {
-    GetMovies(){
+    GetMovies() {
       ApiService.get(
-      `https://api.themoviedb.org/3/search/movie?api_key=18ba471261d253fb5c574c6f1de06e76&query=${this.movie}`
-    ).then((r) => {
-      if (r.status == 200) {
-        this.movies = r.data;
-        this.results = this.movies.results;
-        this.descriptions = this.results.map((s) => ({
-          description: s.overview,
-        }));
-        this.ShowForm = true;
-      } else {
-        console.log(r);
-      }
-      console.log(this.results);
-    });
-    }
+        `https://api.themoviedb.org/3/search/movie?api_key=18ba471261d253fb5c574c6f1de06e76&query=${this.movie}`
+      ).then((r) => {
+        if (r.status == 200) {
+          this.movies = r.data;
+          this.results = this.movies.results;
+          this.descriptions = this.results.map((s) => ({
+            description: s.overview,
+          }));
+          this.ShowForm = true;
+        } else {
+          console.log(r);
+        }
+        console.log(this.results);
+      });
+    },
   },
 };
 </script>
-<style scoped>
-v-menu__content {
-  overflow-y: hidden;
-}
-</style>
+<style scoped></style>
